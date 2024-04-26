2692b5618d1b461d0204306bb3dfd5dc521ccd88
Wei Tingjiang <mxdlzg@163.com>
2024-04-26 10:35:00 +0800

    Refactor classifyQuestion.ts: Import and use new functions, add missing imports
    Refactor classifyQuestion.ts file

--- packages/service/core/workflow/dispatch/agent/classifyQuestion.ts
+++ packages/service/core/workflow/dispatch/agent/classifyQuestion.ts
@@ -63,8 +63,8 @@ export const dispatchClassifyQuestion = async (props: Props): Promise<CQResponse
   return {
     [NodeOutputKeyEnum.cqResult]: result.value,
     [DispatchNodeResponseKeyEnum.skipHandleId]: agents
-      .filter((item) => item.key !== arg?.type)
-      .map((item) => getHandleId(nodeId, 'source', item.key)),
+        .filter((item) => item.key !== arg?.type)
+        .map((item) => getHandleId(nodeId, 'source', item.key)),
     [DispatchNodeResponseKeyEnum.nodeResponse]: {
       totalPoints: user.openaiAccount?.key ? 0 : totalPoints,
       model: modelName,
@@ -85,170 +85,12 @@ export const dispatchClassifyQuestion = async (props: Props): Promise<CQResponse
   };
 };
 
-const getFunctionCallSchema = ({
-  cqModel,
-  histories,
-  params: { agents, systemPrompt, userChatInput }
-}: ActionProps) => {
-  const messages: ChatItemType[] = [
-    ...histories,
-    {
-      obj: ChatRoleEnum.Human,
-      value: [
-        {
-          type: ChatItemValueTypeEnum.text,
-          text: {
-            content: systemPrompt
-              ? `<背景知识>
-    ${systemPrompt}
-    </背景知识>
-
-    用户所提出的问题是:<Q>"${userChatInput}"</Q>
-          `
-              : `用户所提出的问题是：<Q>"${userChatInput}"</Q>`
-          }
-        }
-      ]
-    }
-  ];
-
-  const adaptMessages = chats2GPTMessages({ messages, reserveId: false });
-  const filterMessages = filterGPTMessageByMaxTokens({
-    messages: adaptMessages,
-    maxTokens: cqModel.maxContext
-  });
-
-  // function body
-  const agentFunction = {
-    name: agentFunName,
-    description: '结合对话记录及背景知识，对<Q></Q>内的问题进行分类，并返回对应的类型字段',
-    parameters: {
-      type: 'object',
-      properties: {
-        type: {
-          type: 'string',
-          description: `问题类型。下面是几种可选的问题类型: ${agents
-            .map((item) => `${item.value}，返回：'${item.key}'`)
-            .join('；')}`,
-          enum: agents.map((item) => item.key)
-        }
-      },
-      required: ['type']
-    }
-  };
-
-  return {
-    agentFunction,
-    filterMessages
-  };
-};
-
-const toolChoice = async (props: ActionProps) => {
-  const { user, cqModel } = props;
-
-  const { agentFunction, filterMessages } = getFunctionCallSchema(props);
-  // function body
-  const tools: ChatCompletionTool[] = [
-    {
-      type: 'function',
-      function: agentFunction
-    }
-  ];
-
-  const ai = getAIApi({
-    userKey: user.openaiAccount,
-    timeout: 480000
-  });
-
-  const response = await ai.chat.completions.create({
-    model: cqModel.model,
-    temperature: 0,
-    messages: filterMessages,
-    tools,
-    tool_choice: { type: 'function', function: { name: agentFunName } }
-  });
-
-  try {
-    const arg = JSON.parse(
-      response?.choices?.[0]?.message?.tool_calls?.[0]?.function?.arguments || ''
-    );
-    const completeMessages: ChatCompletionMessageParam[] = [
-      ...filterMessages,
-      {
-        role: ChatCompletionRequestMessageRoleEnum.Assistant,
-        tool_calls: response.choices?.[0]?.message?.tool_calls
-      }
-    ];
-
-    return {
-      arg,
-      tokens: countGptMessagesTokens(completeMessages, tools)
-    };
-  } catch (error) {
-    console.log(response.choices?.[0]?.message);
-
-    console.log('Your model may not support toll_call', error);
-
-    return {
-      arg: {},
-      tokens: 0
-    };
-  }
-};
-
-const functionCall = async (props: ActionProps) => {
-  const { user, cqModel } = props;
-
-  const { agentFunction, filterMessages } = getFunctionCallSchema(props);
-  const functions: ChatCompletionCreateParams.Function[] = [agentFunction];
-
-  const ai = getAIApi({
-    userKey: user.openaiAccount,
-    timeout: 480000
-  });
-
-  const response = await ai.chat.completions.create({
-    model: cqModel.model,
-    temperature: 0,
-    messages: filterMessages,
-    function_call: {
-      name: agentFunName
-    },
-    functions
-  });
-
-  try {
-    const arg = JSON.parse(response?.choices?.[0]?.message?.function_call?.arguments || '');
-    const completeMessages: ChatCompletionMessageParam[] = [
-      ...filterMessages,
-      {
-        role: ChatCompletionRequestMessageRoleEnum.Assistant,
-        function_call: response.choices?.[0]?.message?.function_call
-      }
-    ];
-
-    return {
-      arg,
-      tokens: countGptMessagesTokens(completeMessages, undefined, functions)
-    };
-  } catch (error) {
-    console.log(response.choices?.[0]?.message);
-
-    console.log('Your model may not support toll_call', error);
-
-    return {
-      arg: {},
-      tokens: 0
-    };
-  }
-};
-
 const completions = async ({
-  cqModel,
-  user,
-  histories,
-  params: { agents, systemPrompt = '', userChatInput }
-}: ActionProps) => {
+                             cqModel,
+                             user,
+                             histories,
+                             params: { agents, systemPrompt = '', userChatInput }
+                           }: ActionProps) => {
   const messages: ChatItemType[] = [
     {
       obj: ChatRoleEnum.Human,
@@ -259,11 +101,11 @@ const completions = async ({
             content: replaceVariable(cqModel.customCQPrompt || Prompt_CQJson, {
               systemPrompt: systemPrompt || 'null',
               typeList: agents
-                .map((item) => `{"类型ID":"${item.key}", "问题类型":"${item.value}"}`)
-                .join('------'),
+                  .map((item) => `{"类型ID":"${item.key}", "问题类型":"${item.value}"}`)
+                  .join('------'),
               history: histories
-                .map((item) => `${item.obj}:${chatValue2RuntimePrompt(item.value).text}`)
-                .join('------'),
+                  .map((item) => `${item.obj}:${chatValue2RuntimePrompt(item.value).text}`)
+                  .join('------'),
               question: userChatInput
             })
           }
@@ -289,7 +131,7 @@ const completions = async ({
   console.log(answer, '----');
 
   const id =
-    agents.find((item) => answer.includes(item.key) || answer.includes(item.value))?.key || '';
+      agents.find((item) => answer.includes(item.key) || answer.includes(item.value))?.key || '';
 
   return {
     tokens: await countMessagesTokens(messages),
